2fcb8eca2393b2bd525869a6e5145172e1024688
Lucas Messenger <l.messenger@temperednetworks.com>
2019-03-13 21:11:32 -0700

    Build out dns64 prototype 	modified:   rhino64.go

--- rhino64.go
+++ rhino64.go
@@ -2,42 +2,32 @@ package main
 
 import (
     "github.com/miekg/dns"
-    "fmt"
     "net"
     "log"
-    "os"
     )
+
 
 var (
     prefix = net.IP{0, 0x64, 0xff, 0x9b, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 )
 
+
 func main() {
-    c := new(dns.Client)
-
-    uri := dns.Fqdn(os.Args[1])
-    answers  := makeDNSRequest(c, dns.TypeAAAA, uri)
-    if len(answers) >= 1 {
-        log.Printf("Got IPv6 addresses for %s", uri)
-
-        for _, a := range answers {
-            fmt.Printf(a.(*dns.AAAA).AAAA.String())
-        }
-    } else {
-        answers = makeDNSRequest(c, dns.TypeA, uri)
-        for _, a := range answers {
-            fmt.Printf(a.(*dns.A).A.String())
-        }
+    dns.HandleFunc(".", handleRequest)
+    err := dns.ListenAndServe(":53", "udp6", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
     }
-
 }
 
-func makeDNSRequest(c *dns.Client, rtype uint16, uri string) []dns.RR {
 
-    log.Printf("querying %s for %s\n", rtype, uri)
+func makeRequest(name string, qtype uint16) []dns.RR {
+
+    c := new(dns.Client)
+    log.Printf("querying %s record for %s\n", dns.TypeToString[qtype], name)
 
     m := new(dns.Msg)
-    m.SetQuestion(uri, rtype)
+    m.SetQuestion(name, qtype)
     m.RecursionDesired = true
 
     r, _, err := c.Exchange(m, net.JoinHostPort("8.8.8.8", "53"))
@@ -46,8 +36,65 @@ func makeDNSRequest(c *dns.Client, rtype uint16, uri string) []dns.RR {
     }
 
     if r.Rcode != dns.RcodeSuccess {
-        log.Fatalf("invalid answer name for query %s\n", uri)
+        log.Fatalf("invalid answer name for query %s\n", name)
     }
 
     return r.Answer
 }
+
+
+func handleRequest(w dns.ResponseWriter, req *dns.Msg) {
+
+    m := new(dns.Msg)
+    m.SetReply(req)
+
+    for _, q := range m.Question {
+
+        answers := makeRequest(q.Name, q.Qtype)
+        if len(answers) > 0 {
+            log.Printf("found %v answer(s) for %s", len(answers), q.Name)
+            for _, a := range answers {
+                m.Answer = append(m.Answer, a)
+            }
+        } else {
+            log.Printf("did not find %s answer for %s", dns.TypeToString[q.Qtype], q.Name)
+
+            if q.Qtype == dns.TypeAAAA {
+                log.Printf("generating synthetic IPv6 addr for %s", q.Name)
+
+                answers := makeRequest(q.Name, dns.TypeA)
+                if len(answers) > 0 {
+                    log.Printf("found %v answers for %s", len(answers), q.Name)
+
+                    for _, a := range answers {
+                        rr := &dns.AAAA{
+                            Hdr:  dns.RR_Header{Name: q.Name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 0},
+                            AAAA: makeSyntheticIPv6(a.(*dns.A).A),
+                        }
+                        m.Answer = append(m.Answer, rr)
+                     }
+                }
+            }
+        }
+    }
+
+    buf, _ := m.Pack()
+    w.Write(buf)
+
+}
+
+
+func makeSyntheticIPv6(ip net.IP) net.IP {
+
+    synth := prefix
+    synth[12] = ip[0]
+    synth[13] = ip[1]
+    synth[14] = ip[2]
+    synth[15] = ip[3]
+
+    return synth
+
+}
+
+
+
